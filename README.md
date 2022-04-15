0d9c524222f7d4f5f31026776b6ccd11a1e26cbf
Riculum <riculum@outlook.com>
2022-04-15 11:37:59 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,118 @@
 # PHP-PDO
+A complete database toolkit written in PHP to handle PDO statements
+
+## Installation
+Use the package manager [composer](https://getcomposer.org) to install the library
+```bash
+composer require riculum/php-pdo
+```
+
+## Initial setup
+
+### Credentials
+The basic database settings can be set through environment variables. Add a `.env` file in the root of your project. Make sure the `.env` file is added to your `.gitignore` so it is not checked-in the code. By default, the library looks for the following variables:
+
+* DB_HOST
+* DB_NAME
+* DB_USERNAME
+* DB_PASSWORD
+
+More information how to use environment variables [here](https://github.com/vlucas/phpdotenv)
+
+### Configuration
+Import vendor/autoload.php and load the `.env` settings
+```php
+require_once 'vendor/autoload.php';
+
+use Database\Core\Database as DB;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+```
+
+## Statements
+The `select` statement is used to return an array containing all the result set rows
+```php
+try {
+    $user = DB::select('SELECT * FROM my_table WHERE firstname = ?', ['John']);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+```
+
+The `single` statement is used to fetch the next row from a result set
+```php
+try {
+    $user = DB::single('SELECT * FROM my_table WHERE firstname = ?', ['John']);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+```
+
+The `insert` statement is used to insert new records in a table
+```php
+try {
+    $id = DB::insert('INSERT INTO my_table (firstname, lastname) VALUES (?,?)', ['John', 'Doe']);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+```
+
+The `insertAssoc` statement is used to insert new records in a table using an associative array
+```php
+$data = [
+    'firstname' => 'John',
+    'lastname' => 'Doe'
+];
+
+try {
+    $id = DB::insertAssoc('my_table', $data);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+```
+
+The `update` statement is used to modify the existing records in a table
+```php
+try {
+    DB::update('UPDATE my_table SET firstname = ? WHERE lastname = ?', ['John', 'Doe']);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+```
+
+The `delete` statement is used to delete existing records in a table
+```php
+try {
+    DB::delete('DELETE FROM my_table WHERE firstname = ?', ['John']);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+```
+
+Use `statement` for other operations not mentioned
+```php
+try {
+    DB::statement('DROP TABLE my_table');
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+```
+
+### Transactions
+Transaction are used to run a series of operations within an entity. If an exception is thrown between `beginTransaction` and `commit`, the transaction will automatically be rolled back. 
+```php
+$data = [
+    'firstname' => 'John',
+    'lastname' => 'Doe'
+];
+
+try {
+    DB::beginTransaction();
+    $id = DB::insertAssoc('my_table', $data);
+    DB::update('UPDATE my_table SET firstname = ?, lastname = ? WHERE id = ?', ['Jane', 'Doe', $id]);
+    DB::commit();
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+```
---
+++ core/Database.php
@@ -0,0 +1,124 @@
+<?php
+namespace Database\Core;
+
+use PDO;
+
+class Database
+{
+    private static ?PDO $PDO;
+
+    public static function connect()
+    {
+        if (empty(self::$PDO)) {
+            self::$PDO = new PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+            self::$PDO->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            self::$PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+    }
+
+    public static function beginTransaction()
+    {
+        self::connect();
+        self::$PDO->beginTransaction();
+    }
+
+    public static function commit()
+    {
+        self::$PDO->commit();
+        self::$PDO = null;
+    }
+
+    public static function delete(string $query, $params = [])
+    {
+        self::connect();
+        $statement = self::$PDO->prepare($query);
+        $statement->execute($params);
+    }
+
+    public static function insert(string $query, $params = []): int
+    {
+        self::connect();
+        $statement = self::$PDO->prepare($query);
+        $statement->execute($params);
+        return self::$PDO->lastInsertId();
+    }
+
+    public static function insertAssoc(string $table, $params = []): int
+    {
+        self::connect();
+        $fields = array_keys($params);
+        $values = array_values($params);
+        $fieldList = implode(',', $fields);
+        $qs = str_repeat("?,", count($fields) - 1);
+
+        $sql = "INSERT INTO $table ($fieldList) values($qs?)";
+        $statement = self::$PDO->prepare($sql);
+        $statement->execute($values);
+
+        return self::$PDO->lastInsertId();
+    }
+
+    public static function select(string $query, $params = [], $mode = PDO::FETCH_ASSOC): ?array
+    {
+        self::connect();
+        $statement = self::$PDO->prepare($query);
+        $statement->execute($params);
+        return $statement->fetchAll($mode) ?: [];
+    }
+
+    public static function single(string $query, $params = [], $mode = PDO::FETCH_ASSOC): ?array
+    {
+        self::connect();
+        $statement = self::$PDO->prepare($query);
+        $statement->execute($params);
+        return $statement->fetch($mode) ?: [];
+    }
+
+    public static function statement(string $query, $params = [])
+    {
+        self::connect();
+        $statement = self::$PDO->prepare($query);
+        $statement->execute($params);
+    }
+
+
+    public static function update(string $query, $params = [])
+    {
+        self::connect();
+        $statement = self::$PDO->prepare($query);
+        $statement->execute($params);
+    }
+
+    public static function updateAssoc(string $table, array &$params, array $conditions)
+    {
+        self::connect();
+        $qs = "";
+        $keys = array_keys($params);
+
+        $i = 0;
+        foreach ($keys as $key => $value) {
+            if (++$i !== count($keys)) {
+                $qs .= $value . " = :" . $value . ", ";
+            } else {
+                $qs .= $value . " = :" . $value;
+            }
+        }
+
+        $cs = "";
+
+        if (count($conditions) == count($conditions, COUNT_RECURSIVE)) {
+            $cs = $conditions['key'] . " " . $conditions['operator'] . " :" . $conditions['key'];
+            $params[$conditions['key']] = $conditions['value'];
+        } else {
+            foreach ($conditions as $condition) {
+                $cs .= $condition['key'] . " " . $condition['operator'] . " :" . $condition['key'] . " " . $condition['condition'] . " ";
+                $params[$condition['key']] = $condition['value'];
+            }
+        }
+
+        $sql = "UPDATE $table SET $qs WHERE $cs";
+
+        $statement = self::$PDO->prepare($sql);
+        $statement->execute($params);
+    }
+}
